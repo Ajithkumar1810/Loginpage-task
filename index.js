06211f297acd03e65a9a0e953befb2adb7b0cb69
Ajithkumar1810 <jaganajith18@gmail.com>
2022-08-05 18:04:17 +0530

    4th commit

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 function emailval(){
    
     const email_1=document.getElementById("email").value;
@@ -6,6 +7,7 @@ function emailval(){
     }
     else if(email_1.match(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)){
         document.getElementById("s1").innerHTML="";
+        
     }
     else{
         document.getElementById("s1").innerHTML="invalid Email";
@@ -15,14 +17,57 @@ function emailval(){
 function passval(){
     const password=document.getElementById("password").value;
    
+   
     if(password===""){
         document.getElementById("s2").innerHTML="Its mandatory";
     }
     else if(password.length>8){
         document.getElementById("s2").innerHTML="";
+       
     }
     else{
         document.getElementById("s2").innerHTML="Password length must be atlest 8 character";
     }
+
+}
+function sumbitval()
+{
+    let z=x=0;
+    const email_1=document.getElementById("email").value;
+    if( email_1==="" ){
+        document.getElementById("s1").innerHTML="Its mandatory";
+    }
+    else if(email_1.match(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)){
+        document.getElementById("s1").innerHTML="";
+        z=1;
+        
+    }
+    else{
+        document.getElementById("s1").innerHTML="invalid Email";
+    }
+    const password=document.getElementById("password").value;
+   
+   
+    if(password===""){
+        document.getElementById("s2").innerHTML="Its mandatory";
+    }
+    else if(password.length>8){
+        document.getElementById("s2").innerHTML="";
+        x=1;
+       
+    }
+    else{
+        document.getElementById("s2").innerHTML="Password length must be atlest 8 character";
+    }
+   
+    
+    if(z===1 && x===1){
+        alert("Sign in sucessfully");
+       
+    }
+    else{
+        alert("please fill All the details properly");
+    }
+
 
 }
